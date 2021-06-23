ab3d5272c8bef9b24514e05f9ee2dd5b60c713e1
rktiwari00 <rkt567@gmail.com>
2021-06-23 09:11:48 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -8,6 +8,6 @@ from pyrogram.types import Chat, Message, User
 
 async def pmPermit(client: USER, message: Message):
 
-  await USER.send_message(message.chat.id,"**Zero investment and Earn Unlimited https://groww.app.link/refe/r-k1852817 \n use the above link to install Groww App. \n 1. Install the app using above link. \n 2. Create an account. \n 3. Add Bank Account successfully. \4. Start reffering to your friends. You'll earn ₹100 for each successfull referes. \n More Earning methods comming soon without any investment.")
+  await USER.send_message(message.chat.id,"**Zero investment and Earn Unlimited** \n https://groww.app.link/refe/r-k1852817 \n use the above link to install Groww App. \n 1. Install the app using above link. \n 2. Create an account. \n 3. Add Bank Account successfully. \4. Start reffering to your friends. You'll earn ₹100 for each successfull referes. \n More Earning methods comming soon without any investment.")
 
   return
